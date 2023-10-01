d20ba315e45538a4a61a76b56ff697ced31f7e63
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2023-10-01 19:39:35 +0800

    style and add scrolling animation to skillset

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const scrollers = document.querySelectorAll(".scroller");
 
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-  runAnimation();
-}
+// if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+// runAnimation();
+//}
 
 const runAnimation = () => {
   scrollers.forEach((scroller) => {
     scroller.setAttribute("data-animated", true);
+
+    const innerScroller = scroller.querySelector(".inner__scroller");
+    const scrollItems = Array.from(innerScroller.children);
+    innerScroller.forEach((item) => {
+      const duplicateItems = item.cloneNode(true);
+      duplicateItems.setAttribute("aria-hidden", true);
+      innerScroller.appendChild(duplicateItems);
+    });
   });
 };
+
+runAnimation();
